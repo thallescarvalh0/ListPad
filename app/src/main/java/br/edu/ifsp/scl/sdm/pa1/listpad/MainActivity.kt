219c75c1b0e693c8c1a54cf9a4c6077102ce744d
thallescarvalh0 <thallescarvalhoferreira@gmail.com>
2021-10-16 14:25:17 -0300

    Initial Commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pa1/listpad/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.sdm.pa1.listpad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
