245197ba8ea13b5b71cad998aadcefe5573adfe7
thallescarvalh0 <thallescarvalhoferreira@gmail.com>
2021-10-25 21:48:34 -0300

    Incluindo adapter usando firebaseUI Start stop com funcao de atualizar

--- app/src/main/java/br/edu/ifsp/scl/sdm/pa1/listpad/categoria/CategoriaActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pa1/listpad/categoria/CategoriaActivity.kt
@@ -3,21 +3,24 @@ package br.edu.ifsp.scl.sdm.pa1.listpad.categoria
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.edu.ifsp.scl.sdm.pa1.listpad.R
+import br.edu.ifsp.scl.sdm.pa1.listpad.categoria.adapter.CategoriaAdapter
 import br.edu.ifsp.scl.sdm.pa1.listpad.categoria.model.Categoria
 import br.edu.ifsp.scl.sdm.pa1.listpad.databinding.ActivityCategoriaBinding
+import br.edu.ifsp.scl.sdm.pa1.listpad.utils.DBConstantes
+import br.edu.ifsp.scl.sdm.pa1.listpad.utils.FirebaseInstance
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
 class CategoriaActivity : AppCompatActivity() {
 
-    private val query: Query by lazy {
-        FirebaseFirestore.getInstance().collection("categoria")
-    }
     private val activityCategoriaBinding: ActivityCategoriaBinding by lazy {
         ActivityCategoriaBinding.inflate(layoutInflater)
     }
+
+    lateinit var categoriaAdapter: CategoriaAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +33,39 @@ class CategoriaActivity : AppCompatActivity() {
             startActivity(cadastroCategoriaIntent)
 
         }
-
-        val options = FirestoreRecyclerOptions.Builder<Categoria>().setQuery(query,Categoria::class.java).build()
-
-
     }
 
+    override fun onStart() {
+        super.onStart()
+        updateUI()
+        categoriaAdapter.startListening()
+    }
 
+    override fun onStop() {
+        super.onStop()
+        categoriaAdapter.stopListening()
+    }
+
+    private fun updateUI(){
+        val query: Query = FirebaseInstance.dbFirestore.collection(DBConstantes.TABLE_CATEGORIA).orderBy("descricao")
+        val options: FirestoreRecyclerOptions<Categoria> = FirestoreRecyclerOptions.Builder<Categoria>()
+            .setQuery(query,Categoria::class.java).build()
+
+        categoriaAdapter = CategoriaAdapter(options)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.rvListCategoria)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerview.adapter = categoriaAdapter
+
+        val clickListener = object :CategoriaAdapter.CategoriaClickListener{
+            override fun onItemClick(pos: Int) {
+                //val intent = Intent(applicationContext,DetalheActivity::class.java)
+               // intent.putExtra("contatoID", contatoAdapter.snapshots.getSnapshot(pos).id)
+               // startActivity(intent)
+            }
+
+        }
+
+        categoriaAdapter.clickListener = clickListener
+    }
 }
--- app/src/main/java/br/edu/ifsp/scl/sdm/pa1/listpad/categoria/adapter/CategoriaAdapter.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pa1/listpad/categoria/adapter/CategoriaAdapter.kt
@@ -1,25 +1,38 @@
 package br.edu.ifsp.scl.sdm.pa1.listpad.categoria.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.edu.ifsp.scl.sdm.pa1.listpad.R
 import br.edu.ifsp.scl.sdm.pa1.listpad.categoria.model.Categoria
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import kotlin.time.measureTime
 
 
 class CategoriaAdapter(options: FirestoreRecyclerOptions<Categoria>)
     : FirestoreRecyclerAdapter<Categoria, CategoriaAdapter.CategoriaViewHolder> (options) {
 
+    var clickListener: CategoriaClickListener?=null
+
     inner class CategoriaViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val descricaoCategoria = view.findViewById<TextView>(R.id.tvDescricaoCategoria)
+        init {
+            view.setOnClickListener{clickListener?.onItemClick(bindingAdapterPosition)}
+        }
 
     }
+
+    interface CategoriaClickListener{ fun onItemClick(position: Int) }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): CategoriaAdapter.CategoriaViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.categoria_celula,parent,false)
+        return CategoriaViewHolder(view)
     }
 
     override fun onBindViewHolder(
@@ -27,6 +40,7 @@ class CategoriaAdapter(options: FirestoreRecyclerOptions<Categoria>)
         position: Int,
         model: Categoria
     ) {
-        TODO("Not yet implemented")
+        holder.descricaoCategoria.text = model.descricao
     }
+
 }
